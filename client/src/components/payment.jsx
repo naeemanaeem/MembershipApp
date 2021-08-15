b82405beace9e41e4b98e690f7661a2ad2c4bad3
Wahhaj Malik <wahhaj.malik@gmail.com>
2021-08-14 17:36:19 -0700

    updated payment page

--- client/src/components/payment.jsx
+++ client/src/components/payment.jsx
@@ -35,53 +35,131 @@
 
 // export default payment;
 
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
-import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'; 
+import Modal from 'react-bootstrap/Modal';
 
- 
 class payment extends Component {
  
    render() {
+    function Popup() {
+      const [show, setShow] = useState(false);
+
+      const handleClose = () => setShow(false);
+      const handleShow = () => setShow(true);
+      return (
+        <>
+          <Button variant="primary" onClick={handleShow}>
+            Launch static backdrop modal
+          </Button>
+    
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              I will not close if you click outside me. Don't even try to press
+              escape key.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary">Understood</Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      );
+      <div>
+        {Popup()}
+      </div>
+    }
        return (
        <React.Fragment>
          <br/>
          <Form>
            <Form.Row>
              <Col>
-             <text>Payment</text>
-            <Form.Control as="select">
-              <option> </option>
-              <option>Subscription Fee</option>
-              <option>Donation</option> 
-              <option>Sadaqa</option>
-              <option>Zakat</option>
-            </Form.Control>
+             <h4>Payment</h4>
+
+            <ToggleButtonGroup type="radio" name="options" defaultValue={28} vertical>
+            <ToggleButton id="tbg-radio-1" value={1}
+            variant="outline-primary"
+            style={{height : '60px', width : '400px'}}>
+                  Subscription Fee
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-2" value={2}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Donation
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-3" value={3}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Sadaqah
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-4" value={4}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Zakat
+                </ToggleButton>
+                </ToggleButtonGroup>
+            {/* </ToggleButtonGroup> */}
+            <br/>
+            <br/>
+            <br/>
+            
             </Col>
             <Col>
-            <text>Payment Method</text>
-            <Form.Control as="select">
-              <option> </option>
-              <option>PayPal</option>
-              <option>Bank Deposit</option> 
-              <option>Zelle</option>
-              <option>Venmo</option>
-            </Form.Control>
+            <h4>Payment Method</h4>
+            <ToggleButtonGroup type="radio" name="options" defaultValue={28} vertical>
+            <ToggleButton id="tbg-radio-1" value={1}
+            variant="outline-primary"
+            style={{height : '60px', width : '400px'}}>
+                  PayPal
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-2" value={2}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Bank Deposit
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-3" value={3}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Zelle
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-4" value={4}
+                variant="outline-primary"
+                style={{height : '60px', width : '400px'}}>
+                  Venmo
+                </ToggleButton>
+                </ToggleButtonGroup>
             </Col>
             <Col>
-            <text>Payment Information</text>
+            <h4>Payment Information</h4>
+            <text>Full Name</text>
             <Form.Control
               placeholder="Full Name"
               aria-label="fullname"
               aria-describedby="fullname"
               />
+              <br/>
               <text>Email</text>
               <Form.Control
               placeholder="Email"
               aria-label="email"
               aria-describedby="email"
               />
+              <br/>
               <text>Amount</text>
               <Form.Control
               placeholder="$"
@@ -89,6 +167,7 @@ class payment extends Component {
               aria-describedby="Amount"
               />
               <br/>
+
               <Button variant="primary" type="submit" onClick={this.handleButtonClick}>
             		 	Make Payment
             	</Button>
@@ -97,9 +176,7 @@ class payment extends Component {
             </Form>
        </React.Fragment>
        );
-   }
- 
-
+  }
 }
  
 export default payment;
