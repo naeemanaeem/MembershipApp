4f91acc2a153630bb8d1f7c00695f42cce0eca42
Wahhaj Malik <wahhaj.malik@gmail.com>
2021-10-07 21:13:55 -0700

    added in stripe component (not functioning yet); error in dynamic form for payment

--- client/src/components/payment.jsx
+++ client/src/components/payment.jsx
@@ -19,10 +19,17 @@ import PayPal from "./paypal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import "./table.css";
+import PropTypes from "prop-types";
+// import Stripe from "./stripe";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
 const buttonlist1 = ["Membership Fee", "Donation", "Sadaqah", "Zakat"];
-const buttonlist2 = ["PayPal", "Bank Deposit", "Zelle", "Venmo"];
+const buttonlist2 = ["PayPal", "Card (Stripe)"];
 
+// const PUBLIC_KEY =
+//   "pk_test_51JR4m9CMdg35S26EAT3K6nPEVlxPHubEwzlQ4c2VetzslZmjts2FNQKWxkwZAiQdIgA1kWbCbvmQBGWBrbRONn7a00BSJqSyYd";
+// const stripePromise = loadStripe(PUBLIC_KEY);
 function Payment({ addTextLog }) {
   const [profile, setProfile] = useState({
     Description: "",
@@ -34,6 +41,10 @@ function Payment({ addTextLog }) {
     Status: "Processing",
     Type: "Outgoing",
   });
+
+
+  const [paypal, setPayPal] = useState(false);
+
 
   const [payments, setPayments] = useState([]);
   useEffect(() => {
@@ -98,17 +109,18 @@ function Payment({ addTextLog }) {
                 <ToggleButtonGroup type="radio" name="PaymentMethod"
                   style={{ height: "100%", width: "100%" }}
                   className="ml-5 mr-5">
-                  {buttonlist2.map((buttonLabel, i) => (
-                    <ToggleButton
-                      id={"radio" + i}
-                      value={buttonLabel}
-                      variant="outline-primary"
-                      className="mb-3"
-                      onChange={handleChange}
-                    >
-                      {buttonLabel}
-                    </ToggleButton>
-                  ))}
+                  <ToggleButton id="radio1" value={1}
+                    variant="outline-primary"
+                  //onClick={setPayPal(true)}
+                  >
+                    PayPal
+                  </ToggleButton>
+                  <ToggleButton id="radio2" value={2}
+                    variant="outline-primary"
+                  //onClick={setPayPal(false)}
+                  >
+                    Card (Stripe)
+                  </ToggleButton>
                 </ToggleButtonGroup>
               </Row>
               <h5 className="ml-3 mt-5">User Information</h5>
@@ -186,159 +198,51 @@ function Payment({ addTextLog }) {
               </Row>
               <Row className="middle mb-3">
                 <div className="mt-3">
-                  <PayPal />
+                  {paypal ? (
+                    <PayPal />
+                  ) : (
+                    <Button>Stripe</Button>
+                  )}
+
+                  {/* <Elements stripe={stripePromise}>
+                    <Stripe
+                      profile={profile}
+                      handleSubmitData={handleSubmit}
+                      clientSecret={clientSecret}
+                    />
+                  </Elements> */}
+
                   <Button
                     variant="primary"
                     type="submit"
-                    onClick={() => alert(JSON.stringify(profile, "", 2))}
-                  >
+                    onClick={() => alert(JSON.stringify(profile, "", 2))}>
                     Make Payment
                   </Button>
-                  <Button onClick={() => console.log(payments)}>hello</Button>
+                  {/* <Button onClick={() => console.log(payments)}>hello</Button> */}
                 </div>
               </Row>
             </Card>
           </div>
         </Form>
-        {/* <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col xs="auto">
-              <div className="ml-3 mt-5">
-                <Card style={{ width: "100%", height: "100%" }}>
-                  <div className="ml-3 mt-3 mr-3">
-                    <h4 className="mb-3">Type of Payment</h4>
-                    <ToggleButtonGroup type="radio" name="Description" vertical>
-                      {buttonlist1.map((buttonLabel, i) => (
-                        <ToggleButton
-                          id={"radio" + i}
-                          value={buttonLabel}
-                          variant="outline-primary"
-                          style={{ height: "60px", width: "400px" }}
-                          className="mb-5"
-                          onChange={handleChange}
-                        >
-                          {buttonLabel}
-                        </ToggleButton>
-                      ))}
-                    </ToggleButtonGroup>
-                  </div>
-                </Card>
-              </div>
-            </Col>
-            <Col xs="auto">
-              <div className="ml-3 mt-5">
-                <Card style={{ width: "100%", height: "100%" }}>
-                  <div className="ml-3 mt-3 mr-3" style={{}}>
-                    <h4 className="mb-3">Payment Method</h4>
-                    <ToggleButtonGroup
-                      type="radio"
-                      name="PaymentMethod"
-                      vertical
-                    >
-                      {buttonlist2.map((buttonLabel, i) => (
-                        <ToggleButton
-                          id={"radio" + i}
-                          value={buttonLabel}
-                          variant="outline-primary"
-                          style={{ height: "60px", width: "400px" }}
-                          className="mb-5"
-                          onChange={handleChange}
-                        >
-                          {buttonLabel}
-                        </ToggleButton>
-                      ))}
-                    </ToggleButtonGroup>
-                  </div>
-                </Card>
-              </div>
-            </Col>
-            <Col xs="3">
-              <div className="ml-3 mt-5">
-                <Card style={{ width: "27rem", height: "20rem" }}>
-                  <div className="ml-3 mt-3 mr-3">
-                    <h4>Payment Information</h4>
-                    <Form>
-                      <Row>
-                        <Col>
-                          <div className="mt-3">
-                            <text>First Name</text>
-                            <Form.Control
-                              placeholder="First Name"
-                              value={profile.Firstname}
-                              name="Firstname"
-                              aria-label="firstname"
-                              aria-describedby="firstname"
-                              onChange={handleChange}
-                            />
-                          </div>
-                        </Col>
-                        <Col>
-                          <div className="mt-3">
-                            <text>Last Name</text>
-                            <Form.Control
-                              placeholder="Last Name"
-                              value={profile.Lastname}
-                              name="Lastname"
-                              aria-label="lastname"
-                              type="text"
-                              aria-describedby="lastname"
-                              onChange={handleChange}
-                            />
-                          </div>
-                        </Col>
-                      </Row>
-                      <div className="ml-3 mr-3">
-                        <Row>
-                          <text className="mt-3">Email</text>
-                          <Form.Control
-                            placeholder="Email"
-                            value={profile.Email}
-                            name="Email"
-                            type="text"
-                            aria-label="email"
-                            aria-describedby="email"
-                            onChange={handleChange}
-                          />
-                        </Row>
-                        <Row>
-                          <text className="mt-3">Amount</text>
-                          <Form.Control
-                            placeholder="$"
-                            value={profile.Amount}
-                            name="Amount"
-                            aria-label="Amount"
-                            aria-describedby="Amount"
-                            onChange={handleChange}
-                            type="decimal"
-                          />
-                        </Row>
-                      </div>
-                    </Form>
-                    <br />
-                    <div></div>
-                  </div>
-                </Card>
-              </div>
-              <div className="mt-3">
-                <PayPal />
-                <Button
-                  variant="primary"
-                  type="submit"
-                  onClick={() => alert(JSON.stringify(profile, "", 2))}
-                >
-                  Make Payment
-                </Button>
-                <Button variant="danger" id="clear3" type="refresh">
-                  Clear
-                </Button>
-                <Button onClick={() => console.log(payments)}>hello</Button>
-              </div>
-            </Col>
-          </Row>
-        </Form> */}
       </div>
     </React.Fragment >
   );
 }
 
 export default Payment;
+
+{/* <ToggleButtonGroup type="radio" name="PaymentMethod"
+style={{ height: "100%", width: "100%" }}
+className="ml-5 mr-5">
+{buttonlist2.map((buttonLabel, i) => (
+  <ToggleButton
+    id={"radio" + i}
+    value={buttonLabel}
+    variant="outline-primary"
+    className="mb-3"
+    onChange={handleChange}
+  >
+    {buttonLabel}
+  </ToggleButton>
+))}
+</ToggleButtonGroup> */}
